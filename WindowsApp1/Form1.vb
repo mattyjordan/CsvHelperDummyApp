75bdd856d852c1c04980874c7de53824cb5ad5ab
Matt Jordan <Matt.Jordan@wbd-uk.com>
2022-01-20 15:46:45 +0000

    Add project files.

---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,50 @@
+﻿'Option Strict Off
+
+Imports System.Dynamic
+Imports System.Globalization
+Imports System.IO
+Imports System.Reflection
+Imports CsvHelper
+Imports Newtonsoft.Json
+
+Public Class Form1
+    Public Property CsvHelperExpectedType As Type
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ts = New Timesheet
+
+        CsvHelperExpectedType = ts.GetType()
+
+        Reader(CsvHelperExpectedType)()
+
+    End Sub
+
+    Private Function Reader(Of T As New)() As Type
+        Using theReader = New StreamReader("C:\Users\mattj\OneDrive - Womble Bond Dickinson (UK) LLP\Desktop\2021-11-26 - Timesheets.csv")
+            Using csv = New CsvReader(theReader, CultureInfo.InvariantCulture)
+                Dim records = csv.GetRecords(Of T)().ToList()
+                Dim recordsAsDt = ToDataTable(records)
+            End Using
+        End Using
+    End Function
+
+    Public Function ToDataTable(ByVal items As IEnumerable(Of Object)) As DataTableReader
+        Dim data = items.ToArray()
+        If data.Count() = 0 Then Return Nothing
+        Dim ds = New DataSet()
+        Dim dt = New DataTable()
+
+        For Each key In (CType(data(0), IDictionary(Of String, Object))).Keys
+            dt.Columns.Add(key)
+        Next
+
+        For Each d In data
+            dt.Rows.Add((CType(d, IDictionary(Of String, Object))).Values.ToArray())
+        Next
+
+        ds.Tables.Add(dt)
+
+        Return ds.CreateDataReader()
+    End Function
+
+End Class
---
+++ WindowsApp1/Timesheet.vb
@@ -0,0 +1,55 @@
+﻿Imports CsvHelper.Configuration.Attributes
+
+Public Class Timesheet
+
+    <Index(0)>
+    Public Property [Date] As String
+
+    <Index(1)>
+    Public Property User As String
+
+    <Index(2)>
+    Public Property [UserName] As String
+
+    <Index(3)>
+    Public Property Duration As String
+
+    <Index(4)>
+    Public Property Notes As String
+
+    '<Name("Is Client Related")>
+    <Index(5)>
+    Public Property IsClientRelated As String
+
+    '<Name("Client Name")>
+    <Index(6)>
+    Public Property ClientName As String
+
+    '<Name("Client Reference")>
+    <Index(7)>
+    Public Property ClientReference As String
+
+    <Index(8)>
+    Public Property CRMContactID As String
+
+    '<Name("Time Catagory")>
+    <Index(9)>
+    Public Property TimeCatagory As String
+
+    '<Name("Is Chargeable")>
+    <Index(10)>
+    Public Property IsChargeable As String
+
+    'Is Chargeable
+    '<Name("Amount To Charge")>
+    <Index(11)>
+    Public Property AmountToCharge As String
+
+    '<Name("Potential Amount")>
+    <Index(12)>
+    Public Property PotentialAmount As String
+
+    <Index(13)>
+    Public Property TimeEntryId As String
+
+End Class
